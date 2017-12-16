ae80dae68d4db3c5f71b65c8cb0fb485adf03fce
Salvador Guzman <guzmansalv@gmail.com>
2017-12-15 23:15:07 -0800

    simple reporter of mouse movement

--- src/main/scala/com/github/sguzman/scala/js/Main.scala
+++ src/main/scala/com/github/sguzman/scala/js/Main.scala
@@ -1,16 +1,17 @@
 package com.github.sguzman.scala.js
 
 import org.scalajs.dom
+import rxscalajs.Observable
 
 object Main {
   def main(args: Array[String]): Unit = {
     val canvas = org.scalajs.dom.document.getElementById("canvas")
     val c = canvas.asInstanceOf[org.scalajs.dom.html.Canvas]
     val ctx = c.getContext("2d").asInstanceOf[dom.CanvasRenderingContext2D]
-    println(canvas.clientWidth)
-    println(canvas.clientHeight)
-    ctx.moveTo(0, 0)
-    ctx.lineTo(canvas.clientWidth, canvas.clientHeight)
-    ctx.stroke()
+    Observable.fromEvent(dom.document.body, "mousemove")
+        .map(_.asInstanceOf[dom.MouseEvent])
+      .subscribe(s => {
+        println(s.clientX, s.clientY)
+      })
   }
 }
