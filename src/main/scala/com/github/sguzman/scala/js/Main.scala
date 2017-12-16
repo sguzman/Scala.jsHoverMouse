9aeeb08ca51202cbd8ae671aace47e02067a5c15
Salvador Guzman <guzmansalv@gmail.com>
2017-12-15 23:27:13 -0800

    what a trippy experience

--- src/main/scala/com/github/sguzman/scala/js/Main.scala
+++ src/main/scala/com/github/sguzman/scala/js/Main.scala
@@ -3,15 +3,19 @@ package com.github.sguzman.scala.js
 import org.scalajs.dom
 import rxscalajs.Observable
 
+import scala.util.Random
+
 object Main {
   def main(args: Array[String]): Unit = {
     val canvas = org.scalajs.dom.document.getElementById("canvas")
     val c = canvas.asInstanceOf[org.scalajs.dom.html.Canvas]
     val ctx = c.getContext("2d").asInstanceOf[dom.CanvasRenderingContext2D]
     Observable.fromEvent(dom.document.body, "mousemove")
-        .map(_.asInstanceOf[dom.MouseEvent])
+      .map(_.asInstanceOf[dom.MouseEvent])
+      .map(s => (s.clientX, s.clientY))
       .subscribe(s => {
-        println(s.clientX, s.clientY)
+        ctx.fillStyle = s"rgb(${Random.nextInt(256)}, ${Random.nextInt(256)}, ${Random.nextInt(256)})"
+        ctx.fillRect(Random.nextInt(200), 0, 200, 200)
       })
   }
 }
