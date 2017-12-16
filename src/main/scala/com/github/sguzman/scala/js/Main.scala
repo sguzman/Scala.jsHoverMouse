b73aa69c62f789b2f250062c6feb35d04deaea32
Salvador Guzman <guzmansalv@gmail.com>
2017-12-15 22:28:59 -0800

    draw simple line

--- src/main/scala/com/github/sguzman/scala/js/Main.scala
+++ src/main/scala/com/github/sguzman/scala/js/Main.scala
@@ -1,5 +1,16 @@
 package com.github.sguzman.scala.js
 
+import org.scalajs.dom
+
 object Main {
-  def main(args: Array[String]): Unit = println("Hello world")
+  def main(args: Array[String]): Unit = {
+    val canvas = org.scalajs.dom.document.getElementById("canvas")
+    val c = canvas.asInstanceOf[org.scalajs.dom.html.Canvas]
+    val ctx = c.getContext("2d").asInstanceOf[dom.CanvasRenderingContext2D]
+    println(canvas.clientWidth)
+    println(canvas.clientHeight)
+    ctx.moveTo(0, 0)
+    ctx.lineTo(canvas.clientWidth, canvas.clientHeight)
+    ctx.stroke()
+  }
 }
